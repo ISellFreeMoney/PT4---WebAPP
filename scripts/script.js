dac86f333eaf3320fbd27af68ac73e40bf6fece3
Victor LAUMOND <v1ct0rl@protonmail.com>
2022-03-01 15:37:08 +0100

    Adding style and model for the image identification

--- scripts/script.js
+++ scripts/script.js
@@ -2,26 +2,35 @@ const dropArea = document.querySelector('.drag-area'),
     button = document.querySelector('button'),
     input = document.querySelector('input'),
     dragText = document.querySelector('header');
-let file; // variable globale qui contiendra le fichier
+
+const model = tf.loadLayersModel('model.json');
+
+// variable globale qui contiendra le fichier
+let file;
 
 button.onclick = () => {
-    input.click(); // Si l'utilisateur clique sur le bouton, on lui demande de choisir un fichier
+    // Si l'utilisateur clique sur le bouton, on lui demande de choisir un fichier
+    input.click();
 };
+
 
 input.addEventListener('change', function(){
     file = this.files[0]; // On récupère le fichier choisi
     dropArea.classList.add('active');
     affFile();
+    guessImage()
 });   
+
 
 // Si l'utiliateur passe au dessus de la zone de drop
 dropArea.addEventListener('dragover', (e) => {
-
-    e.preventDefault(); // Annule le comportement par défaut du navigateur
+    // Annule le comportement par défaut du navigateur
+    e.preventDefault();
     //console.log('dragover');
     dropArea.classList.add('active');
     dragText.textContent = ' Lachez le fichier pour deposer ';
 });
+
 
 // Si l'utilisateur sort de la zone de drop
 dropArea.addEventListener('dragleave', () => {
@@ -38,25 +47,54 @@ dropArea.addEventListener('drop', (e) => {
     // Recuperer le fichier et [0] pour recuperer le premier fichier
     file = e.dataTransfer.files[0];
     affFile();
+    guessImage();
 });
 
 function affFile(){
     let fileType = file.type;
     //console.log(fileType);
     // Verifier le type de fichier
-    let validType = ['image/png', 'image/jpeg', "image/webp", "image/jpg"]; // tableau de type de fichier valides
-    if (validType.includes(fileType)) { // si le type de fichier est valide
-        let reader = new FileReader(); // creer un objet de type FileReader
+    // tableau de type de fichier valides
+    let validType = ['image/png', 'image/jpeg', "image/webp", "image/jpg"];
+
+    // si le type de fichier est valide
+    if (validType.includes(fileType)) {
+        // creer un objet de type FileReader
+        let reader = new FileReader();
         reader.onload = () => {
             let fileURL = reader.result;
-            let imgTag = `<img src="${fileURL}" alt="">`; // creer une balise img avec l'url du fichier
-            dropArea.innerHTML = imgTag; // afficher l'image dans la zone de drop
+            // creer une balise img avec l'url du fichier
+            let imgTag = `<img src="${fileURL}" alt="">`;
+            // afficher l'image dans la zone de dro
+            dropArea.innerHTML = imgTag;
         }
-        reader.readAsDataURL(file); // Fichier traduit en base64
-        //console.log('Type valide');
-    } else { // si le type de fichier n'est pas valide
+        reader.readAsDataURL(file);
+    }
+    // si le type de fichier n'est pas valide
+    else {
         alert('Fichier invalide: ' + fileType);
         dropArea.classList.remove('active');
     }
+}
+
+const classes=['Asterix','Obelix']
+
+function guessImage(){
+    model.then(function (res) {
+        // Convert the image into the right TensorFlow format
+        let example = tf.browser.fromPixels(file).expandDims();
+        // Change the size
+        example = tf.file.resizeBilinear(example, [150, 150]).div(tf.scalar(255))
+        // Cast to float
+        example = tf.cast(example, dtype = 'float32');
+
+        // Predict the class
+        let prediction = res.predict(example);
+        figurineClass=Math.round (prediction.dataSync());
+        console.log(figurineClass);
+
+    }, function (err) {
+        console.log(err);
+    });
 }
 
